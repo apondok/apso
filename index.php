278dbac514b0740612f28d1b91a7c562c5bc5be5
apondok <apondok@gmail.com>
2024-08-09 16:28:08 -0400

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 $selected_county = isset($_GET['selection']) ? $_GET['selection'] : '911';
 
 // Include the JSON reading logic
-$json_path = '/properties/' . $selected_county . '_data.json';
+$json_path = 'properties/' . $selected_county . '_data.json';
 $json = file_get_contents($json_path);
 $data = json_decode($json, true);
 
@@ -17,77 +17,51 @@ $properties = $data['data']['cat1']['searchResults']['listResults'];
 
             <div id="slider" class="sl-slider-wrapper">
 
-        <div class="sl-slider">
-		
-	        <div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
-	            <div class="sl-slide-inner">
-	              <div class="bg-img bg-img-1"></div>
-	              <h2><a href="#">2 Bed Rooms and 1 Dinning Room Aparment on Sale</a></h2>
-	              <blockquote>              
-	              <p class="location"><span class="glyphicon glyphicon-map-marker"></span> 1890 Syndey, Australia</p>
-	              <p>Until he extends the circle of his compassion to all living things, man will not himself find peace.</p>
-	              <cite>$ 20,000,000</cite>
-	              </blockquote>
-	            </div>
-	          </div>
-	          
-	          <div class="sl-slide" data-orientation="vertical" data-slice1-rotation="10" data-slice2-rotation="-15" data-slice1-scale="1.5" data-slice2-scale="1.5">
-	            <div class="sl-slide-inner">
-	              <div class="bg-img bg-img-2"></div>
-	              <h2><a href="#">2 Bed Rooms and 1 Dinning Room Aparment on Sale</a></h2>
-	              <blockquote>              
-	              <p class="location"><span class="glyphicon glyphicon-map-marker"></span> 1890 Syndey, Australia</p>
-	              <p>Until he extends the circle of his compassion to all living things, man will not himself find peace.</p>
-	              <cite>$ 20,000,000</cite>
-	              </blockquote>
-	            </div>
-	          </div>
-	          
-	          <div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="3" data-slice2-rotation="3" data-slice1-scale="2" data-slice2-scale="1">
-	            <div class="sl-slide-inner">
-	              <div class="bg-img bg-img-3"></div>
-	              <h2><a href="#">2 Bed Rooms and 1 Dinning Room Aparment on Sale</a></h2>
-	              <blockquote>              
-	              <p class="location"><span class="glyphicon glyphicon-map-marker"></span> 1890 Syndey, Australia</p>
-	              <p>Until he extends the circle of his compassion to all living things, man will not himself find peace.</p>
-	              <cite>$ 20,000,000</cite>
-	              </blockquote>
-	            </div>
-	          </div>
-	          
-	          <div class="sl-slide" data-orientation="vertical" data-slice1-rotation="-5" data-slice2-rotation="25" data-slice1-scale="2" data-slice2-scale="1">
-	            <div class="sl-slide-inner">
-	              <div class="bg-img bg-img-4"></div>
-	              <h2><a href="#">2 Bed Rooms and 1 Dinning Room Aparment on Sale</a></h2>
-	              <blockquote>              
-	              <p class="location"><span class="glyphicon glyphicon-map-marker"></span> 1890 Syndey, Australia</p>
-	              <p>Until he extends the circle of his compassion to all living things, man will not himself find peace.</p>
-	              <cite>$ 20,000,000</cite>
-	              </blockquote>
-	            </div>
-	          </div>
-	          
-	          <div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1">
-	            <div class="sl-slide-inner">
-	              <div class="bg-img bg-img-5"></div>
-	              <h2><a href="#">2 Bed Rooms and 1 Dinning Room Aparment on Sale</a></h2>
-	              <blockquote>              
-	              <p class="location"><span class="glyphicon glyphicon-map-marker"></span> 1890 Syndey, Australia</p>
-	              <p>Until he extends the circle of his compassion to all living things, man will not himself find peace.</p>
-	              <cite>$ 20,000,000</cite>
-	              </blockquote>
-	            </div>
-	          </div>
-          
-        </div><!-- /sl-slider -->
+				<div class="sl-slider">
+				
+				  <div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
+					<div class="sl-slide-inner">
+					  <div class="bg-img bg-img-1"></div>
+					  <h2><a href="#"></a></h2>
+					</div>
+				  </div>
+				  
+				  <div class="sl-slide" data-orientation="vertical" data-slice1-rotation="10" data-slice2-rotation="-15" data-slice1-scale="1.5" data-slice2-scale="1.5">
+					<div class="sl-slide-inner">
+					  <div class="bg-img bg-img-2"></div>
+					  <h2><a href="#"></a></h2>
+					</div>
+				  </div>
+				  
+				  <div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="3" data-slice2-rotation="3" data-slice1-scale="2" data-slice2-scale="1">
+					<div class="sl-slide-inner">
+					  <div class="bg-img bg-img-3"></div>
+					  <h2><a href="#"></a></h2>
+					</div>
+				  </div>
+				  
+				  <div class="sl-slide" data-orientation="vertical" data-slice1-rotation="-5" data-slice2-rotation="25" data-slice1-scale="2" data-slice2-scale="1">
+					<div class="sl-slide-inner">
+					  <div class="bg-img bg-img-4"></div>
+					  <h2><a href="#"></a></h2>
+					</div>
+				  </div>
+				  
+				  <div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1">
+					<div class="sl-slide-inner">
+					  <div class="bg-img bg-img-5"></div>
+					  <h2><a href="#"></a></h2>
+					</div>
+				  </div>
+				</div><!-- /sl-slider -->
 
-        <nav id="nav-dots" class="nav-dots">
-          <span class="nav-dot-current"></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </nav>
+				<nav id="nav-dots" class="nav-dots">
+				  <span class="nav-dot-current"></span>
+				  <span></span>
+				  <span></span>
+				  <span></span>
+				  <span></span>
+				</nav>
 
       </div><!-- /slider-wrapper -->
 </div>
@@ -97,9 +71,7 @@ $properties = $data['data']['cat1']['searchResults']['listResults'];
     <!-- banner -->
         </div>
       </div>
-    </div>
-  </div>
-</div>
+
 <!-- banner -->
 <div class="container">
 	<p></p>
