0f1ad2383f3eb67c65a04af058cb5ac3f42e0241
apondok <apondok@gmail.com>
2024-08-09 16:36:26 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Realestate Bootstrap Theme </title>
+<title>TML Spell Properties</title>
 <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
@@ -58,6 +58,7 @@
                 <li><a href="about.php">About</a></li>
                 <li><a href="agents.php">Agents</a></li>         
                 <li><a href="blog.php">Blog</a></li>
+                <li><a href="contact.php">Contact</a></li>
               </ul>
             </div>
             <!-- #Nav Ends -->
@@ -78,6 +79,11 @@
 <div class="header">
 <a href="index.php"><img src="images/logo.png" alt="Realestate"></a>
 
+              <ul class="pull-right">
+                <li><a href="buysalerent.php">Buy</a></li>
+                <li><a href="buysalerent.php">Sale</a></li>         
+                <li><a href="buysalerent.php">Rent</a></li>
+              </ul>
 </div>
 <!-- #Header Starts -->
 </div>
